be876671473bd91d756a7c04d28bf7f788252c67
Sindre Thormodsæter Benonisen <benonisen@gmail.com>
2023-09-14 13:52:27 +0200

    fixed redirect

--- src/Image.jsx
+++ src/Image.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { useNavigate } from 'react-router-dom';
-
 
 function Input ({value, cb, placeholder}) {
 
@@ -17,9 +15,6 @@ function Input ({value, cb, placeholder}) {
 }
 
 function App() {
-
-  const navigate = useNavigate();
-
   const [d, setD] = useState("")
   const [u, setU] = useState("")
   const [a, setA] = useState("")
@@ -27,16 +22,8 @@ function App() {
   const [e, setE] = useState("")
   const isValid = (d+u+a+l+e).toLowerCase() === "duale";
 
-  if(isValid){
-    window.location.href = 'https://hvilepust.com/tretoppiglo-ved-lardalselva/';
-  }
-
   return <>
-    <Input value={d} cb={setD} placeholder={"1"} />
-    <Input value={u} cb={setU} placeholder={"2"} />
-    <Input value={a} cb={setA} placeholder={"3"}/>
-    <Input value={l} cb={setL} placeholder={"4"} />
-    <Input value={e} cb={setE} placeholder={"5"}/>
+    Vi skal til!
    </>
 }
 
